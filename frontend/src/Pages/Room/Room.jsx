2723e3c7b38a2da852700d675fa62c826d8be48e
Marcos Raimondi <marcosraimondi1@hotmail.com>
2022-06-29 20:53:53 -0300

    room code title

--- frontend/src/Pages/Room/Room.jsx
+++ frontend/src/Pages/Room/Room.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 export default function Room() {
+  const [roomCode, setRoomCode] = useState("");
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    setRoomCode(urlParams.get("code"));
+  }, []);
   return (
     <>
-      <Header id="spotify-masthead" title="User's Room" description="Vote for next song">
+      <Header id="spotify-masthead" title={`Room ${roomCode}`} description="Vote for next song">
         <button>OPTION 1</button>
-        <button>OPTION 1</button>
+        <button>OPTION 2</button>
       </Header>
       <Footer />
     </>
