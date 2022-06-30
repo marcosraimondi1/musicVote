7fa404aec2a737b9770f6c69ad8b95df3771f454
Marcos Raimondi <marcosraimondi1@hotmail.com>
2022-06-30 18:15:12 -0300

    json parse session

--- backend/api/middlewares/authentication.js
+++ backend/api/middlewares/authentication.js
@@ -1,7 +1,8 @@
 const { generateRandomString } = require("../helper/fetchApi.js");
 
 const checkSession = (req, res, next) => {
-  let session = req.get("session");
+  let session = JSON.parse(req.get("session"));
+
   if (!session || !session.id) {
     // create new session
     req.session = {
