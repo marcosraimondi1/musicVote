50c05cd18fb272769136ea792d7937a04bd8034f
Marcos Raimondi <marcosraimondi1@hotmail.com>
2022-07-05 17:06:22 -0300

    mongo container

--- backend/api/mongo_config.js
+++ backend/api/mongo_config.js
@@ -2,16 +2,21 @@ const mongoose = require("mongoose");
 
 function mongo_config() {
   // Mongo Connection
+  const ip = process.env.IP;
+  const mongoDatabase = process.env.MONGO_DATABASE;
   const mongoUserName = process.env.MONGO_USER;
   const mongoPassword = process.env.MONGO_PASS;
+  const mongoPort = process.env.MONGO_PORT;
 
   // for mongodb Atlas: mongodb+srv://devuser:<password>@cluster0.xgchr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-  const uri =
-    "mongodb+srv://" +
-    mongoUserName +
-    ":" +
-    mongoPassword +
-    "@cluster0.xgchr.mongodb.net/turnero_db?retryWrites=true&w=majority";
+  // const uri =
+  //   "mongodb+srv://" +
+  //   mongoUserName +
+  //   ":" +
+  //   mongoPassword +
+  //   "@cluster0.xgchr.mongodb.net/turnero_db?retryWrites=true&w=majority";
+
+  const uri = `mongodb://${mongoUserName}:${mongoPassword}@${ip}:${mongoPort}/${mongoDatabase}?retryWrites=true&w=majority`;
 
   const options = {
     useNewUrlParser: true,
